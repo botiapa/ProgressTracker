037c00d88c07e1beba426318a7cfbe16bccdc7da
KillSwitch <asd>
2019-09-06 20:34:11 +0200

    added some json objects

--- APIProgressTracker/JSONObjects/Author.cs
+++ APIProgressTracker/JSONObjects/Author.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace APIProgressTracker.JSON
+namespace APIProgressTracker.JSONObjects
 {
-    class Message
+    class Author
     {
 
     }
