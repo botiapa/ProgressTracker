aa7103ce06962ad826761e562c23602302e6d9c7
Peter Gabor <peter.gabor04@gmail.com>
2019-09-06 20:39:16 +0200

    Asd

--- APIProgressTracker/ProgressTrackerAPI.cs
+++ APIProgressTracker/ProgressTrackerAPI.cs
@@ -1,9 +1,32 @@
 ﻿using System;
+using Newtonsoft;
 
 namespace APIProgressTracker
 {
-    public class ProgressTrackerAPI
+    public abstract class ProgressTrackerAPI
     {
+        private const string SERVER = "www.remotemysql.com";
+        private const string DATABASE = "progresstracker";
+        private const string UID = "TMrArME2SE";
+        private const string PASSWORD = "kJTKB2snGQ";
 
+        ProgressTrackerAPI()
+        {
+
+        }
+
+        public void Initialize()
+        {
+            //TODO: Connect
+
+        }
+    
+        #region Actual API calls
+        public void CreateNewMessage(Message message)
+        {
+            // var asd = Json.Encode(message);
+            // messagesTable.add(ID, asd);
+        }
+        #endregion
     }
 }
