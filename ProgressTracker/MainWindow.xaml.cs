123a52084e942d849679a5a0617a6f96ce64f099
Peter Gabor <peter.gabor04@gmail.com>
2019-09-06 18:58:41 +0200

    add project

---
+++ ProgressTracker/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace ProgressTracker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_connect_click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string conString = "datasource=https://remotemysql.com;username=TMrArME2SE;password=kJTKB2snGQ";
+                MySqlConnection conn = new MySqlConnection(conString);
+            }
+        }
+    }
+}
